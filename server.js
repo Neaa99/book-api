1aeb85243392040c81cf19f93b7518a79fa05f65
Neaa99 <linnea@kringelkroken.se>
2022-06-13 13:39:36 +0200

    categories

--- server.js
+++ server.js
@@ -274,6 +274,19 @@ app.get('/marvel/categories/:category', async (req, res) => {
   }
 })
 
+app.get('/marvel/categories', async (req, res) => {
+  try {
+    const marvelCategory = await Marvel.find({ category: req.params})
+    if (marvelCategory.length === 0) {
+      res.status(404).json({error: 'Category not found'})
+    } else {
+      res.json(marvelCategory)
+    }
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid Category'})
+  }
+})
+
 // Find marvel by id
 app.get(`/marvel/:id`, async (req, res) => {
   try {
