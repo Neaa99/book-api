bd0319c517b9ed693dc59cfc195aa2bba455b65d
Neaa99 <linnea@kringelkroken.se>
2022-06-13 13:08:20 +0200

    yes auth

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import listEndpoints from "express-list-endpoints";
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
 
 import data from './data/marvel.json'
 
@@ -17,6 +19,147 @@ mongoose.Promise = Promise;
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors());
+app.use(express.json());
+
+// Auth ///
+const User = mongoose.model('User', {
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  fullName: {
+    type: String,
+    unique: false
+  },
+  age: {
+    type: Number,
+    unique: false
+  },
+  location: {
+    type: String,
+    unique: false
+  },
+  description: {
+    type: String,
+    unique: false
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({accessToken: req.header('Authorization')})
+  if (user) {
+    req.user = user
+    next()
+  } else {
+    res.status(401).json({loggedOut: true})
+  }
+}
+
+app.get('/sessions/:AuthId', authenticateUser)
+app.get('/sessions/:AuthId', async (req, res) => {
+  const { AuthId } = req.params
+
+  try {
+    const user = await User.findById(AuthId)
+    if (user) {
+      res.status(201).json({ email: user.email, fullName: user.fullName, age: user.age, location: user.location, description: user.description })
+    } else {
+      res.status(404).json({ success: false, message: 'Could not find profile information' })
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
+  
+})
+
+app.post('/signup', async (req, res) => {
+  const { username, password, email } = req.body
+
+  try {
+    const salt = bcrypt.genSaltSync()
+
+    const newUser = await new User({ 
+      username, 
+      password: bcrypt.hashSync(password, salt),
+      email 
+    }).save()
+    
+    res.status(201).json({ 
+      success: true,
+      AuthId: newUser._id, 
+      username: newUser.username, 
+      email: newUser.email,
+      accessToken: newUser.accessToken, 
+    })
+  } catch (error) {
+    if (error.code === 11000) {
+      res.status(400).json({ message: 'User already exists', fields: error.keyValue })
+    }
+    res.status(400).json({ success: false, message: 'Could not create user', error })
+  }
+})
+
+app.post('/sessions', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const user = await User.findOne({ username })
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.json({ 
+        success: true, 
+        AuthId: user._id, 
+        username: user.username, 
+        email: user.email, 
+        accessToken: user.accessToken, 
+        fullName: user.fullName, 
+        age: user.age, 
+        location: user.location, 
+        description: user.description 
+      })
+    } else {
+      res.status(404).json({ success: false, message: 'Could not find user' })
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, message: 'Invalid request', error })
+  }
+})
+
+app.patch('/sessions/:AuthId', authenticateUser)
+app.patch('/sessions/:AuthId', async (req, res) => {
+  const { AuthId } = req.params
+
+  try {
+    const updateUser = await User.findByIdAndUpdate(AuthId, req.body, { new: true })
+
+    if (updateUser) {
+      res.json({ success: true, updateUser })
+    } else {
+      res.status(404).json({ success: false, message: 'Not found' })
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
+})
+
+// Marvel //
 
 const Marvel = mongoose.model("Marvel", {
   "title": String,
@@ -46,9 +189,7 @@ if (process.env.RESET_DB === 'true') {
   seedDatabase()
 }
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+
 
 // ERROR HANDLING:
 app.use((req, res, next) => {
