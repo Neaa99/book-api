f8f6ad08f6ebeb1086be6e021874c9dc18553a6a
Neaa99 <linnea@kringelkroken.se>
2022-06-15 13:47:18 +0200

    medium endpoint

--- server.js
+++ server.js
@@ -274,18 +274,18 @@ app.get('/marvel/categories/:category', async (req, res) => {
   }
 })
 
-// app.get('/marvel/categories/all', async (req, res) => {
-//   try {
-//     const marvelCategory = await Marvel.find({ category: req.params})
-//     if (marvelCategory.length === 0) {
-//       res.status(404).json({error: 'Category not found'})
-//     } else {
-//       res.json(marvelCategory)
-//     }
-//   } catch (err) {
-//     res.status(400).json({ error: 'Invalid Category'})
-//   }
-// })
+app.get('/marvel/medium/:medium', async (req, res) => {
+  try {
+    const marvelMedium = await Marvel.find({ medium: req.params.medium})
+    if (marvelMedium.length === 0) {
+      res.status(404).json({error: 'Medium not found'})
+    } else {
+      res.json(marvelMedium)
+    }
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid medium'})
+  }
+})
 
 // Find marvel by id
 app.get(`/marvel/:id`, async (req, res) => {
